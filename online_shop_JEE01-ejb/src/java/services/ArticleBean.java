23a02d78ef9bda948d84583c1c8c36cb7df6c39a
Mick <Mick@mydom.de>
2014-09-30 16:38:53 +0200

    3. Test Branch

--- online_shop_JEE01-ejb/src/java/services/ArticleBean.java
+++ online_shop_JEE01-ejb/src/java/services/ArticleBean.java
@@ -19,32 +19,36 @@ import persistence.Article;
 @Stateless
 public class ArticleBean implements ArticleServiceLocal, ArticleServiceRemote {
 
-	@PersistenceContext(unitName="online_shop_JEE01-ejbPU")
-	EntityManager em;
-	
-	@Override
-	public List<Article> getAll() {
-		return new Article().findAll(em);
-	}
+  @PersistenceContext(unitName = "online_shop_JEE01-ejbPU")
+  EntityManager em;
 
-	@Override
-	public void insertNew(Article article) {
-		System.out.println("BEAN insertNew:" + article);
-		em.persist(article);
-	}
-
-	@Override
-	public void updateExist(Article article) {
-		System.out.println("BEAN updateExist:" + article);
-		em.merge(article);
-	}
-
-  private void test(){
-    
+  @Override
+  public List<Article> getAll() {
+    return new Article().findAll(em);
   }
 
-  private void test2(){
-    
+  @Override
+  public void insertNew(Article article) {
+    System.out.println("BEAN insertNew:" + article);
+    em.persist(article);
+  }
+
+  @Override
+  public void updateExist(Article article) {
+    System.out.println("BEAN updateExist:" + article);
+    em.merge(article);
+  }
+
+  private void test() {
+
+  }
+
+  private void test2() {
+
+  }
+
+  private void test3() {
+
   }
 
 }
